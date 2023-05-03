2c87e3830555912c76e36c8d2bc90c0c74b37c8b
pavel <pppirogov21@gmail.com>
2023-05-03 17:39:12 +0800

    3.1 Методы __setattr__, __getattribute__, __getattr__ и __delattr__

---
+++ steps/ex.py
@@ -0,0 +1,18 @@
+s ='python---eto-----kru--to!'
+answer =''
+b =len(s)
+#print(b)
+a = 0
+while  a <  b:
+    #print(s[a],a)
+    if s[a]!='-':
+        answer+=s[a]
+    else:
+        count = 1
+        while a + count != b and s[a+count]=='-':
+            count+=1
+        a+=count-1
+        answer += '-'
+
+    a+=1
+print(answer)
--- steps/testt.py
+++ steps/testt.py
@@ -1,6 +1,3 @@
-lst = ["Пункт меню 1", "Пункт меню 2", "Пункт меню 3"]
-answer = f'<>'
-for a in lst:
-    answer+=a
-    answer+=f'<dsad>\n'
-print (answer)
+s = input()
+lst = list(map(lambda s: abs(int(s)), input.split()))
+print(*lst)
